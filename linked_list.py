94366b94bb95cb1a95a72a6e1c7fcb7e80391540
Windula Dissanayake <charithwinwindula@gmail.com>
2024-05-31 17:15:56 +0530

    initial-commit

---
+++ linked_list.py
@@ -0,0 +1,18 @@
+class Node:
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+    
+
+N1 = Node(10)
+# print(N1.data)
+print(N1)
+
+N2 = Node(20)
+N1.next_node = N2
+print(N1.next_node)
