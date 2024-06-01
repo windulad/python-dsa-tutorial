ff659ce6d3340f7073094020dc487763b126a2b3
Windula Dissanayake <charithwinwindula@gmail.com>
2024-06-01 12:11:27 +0530

    file-changes

--- linked_list.py
+++ linked_list.py
@@ -9,10 +9,9 @@ class Node:
         return "<Node data: %s>" % self.data
     
 
-N1 = Node(10)
-# print(N1.data)
-print(N1)
+# N1 = Node(10)
+# print(N1)
 
-N2 = Node(20)
-N1.next_node = N2
-print(N1.next_node)
+# N2 = Node(20)
+# N1.next_node = N2
+# print(N1.next_node)
