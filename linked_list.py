9d07ad7c390588974805cb3dae9f057095aa1421
Windula Dissanayake <charithwinwindula@gmail.com>
2024-06-01 22:19:38 +0530

    edit linked list

--- linked_list.py
+++ linked_list.py
@@ -30,11 +30,65 @@ class LinkedList:
 
         return count
     
-    # Implement add node to Linked List
+    # Implement add (add from front) node to Linked List
     def add(self, data):
         new_node = Node(data)       # Create a new Node
         new_node.next_node = self.head      # Pointer of new Node to Head of previous Linked List 
         self.head = new_node        # Pointer to new Linked List
+
+    # Implement search to Linked List
+    def search(self, key):
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+    
+    # Implement insert to Linked List
+    def insert(self, data, index):
+        # Add index 0 node to front of Linked List
+        if index == 0:
+            self.add(data)
+
+        # Insert index != 0 to Linked List
+        if index > 0:
+            new = Node(data)        # Create new Node
+
+            position = index        # Assign index to position
+            current = self.head
+
+            while position > 1:
+                current = current.next_node        # Traverse through Linked List from Head to Tail until index found
+                position -= 1
+
+            prev_node = current             # Assign current node in index to previous
+            next_node = current.next_node   # Assign next node in index to next
+
+            prev_node.next_node = new       # Assign next node of previous as new node
+            new.next_node = next_node       # Assign next node of new node as next node
+
+    # Implement delete to Linked List
+    def remove(self, key):
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            # If key is head
+            if current.data == key and current == self.head:
+                found = True
+                self.head = current.next_node       # Delete head and assign to next node
+            # If key is not head
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node      # Delete current node and assign pointer to next node
+            else:
+                previous = current
+                current = current.next_node
+        return current
 
     # Implement representing method for the Linked List
     def __repr__(self):
@@ -52,20 +106,6 @@ class LinkedList:
 
         return '->'.join(nodes)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # N1 = Node(10)
 # print(N1)
 # N2 = Node(20)
@@ -77,8 +117,26 @@ class LinkedList:
 # L.head = N1
 # print(L.size())
 
+# L = LinkedList()
+# L.add(1)
+# L.add(2)
+# L.add(3)
+# print(L)
+
+# L = LinkedList()
+# L.add(10)
+# L.add(2)
+# L.add(45)
+# L.add(15)
+# n = L.search(44)
+# print(n)
+
 L = LinkedList()
-L.add(1)
+L.add(10)
 L.add(2)
-L.add(3)
+L.add(45)
+L.add(15)
+L.add(67)
+print(L)
+L.remove(2)
 print(L)
